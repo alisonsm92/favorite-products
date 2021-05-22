5724c75be604d90e1bfb784ef42ca9a6f844e5fe
Alison Machado <alison.sm92@gmail.com>
2021-05-22 20:27:47 -0300

    feat: add create costumer controller factory

---
+++ src/app/factory/controller/create-costumer-controller-factory.ts
@@ -0,0 +1,10 @@
+import CreateCustomerController from '../../../adapter/input/create-customer-controller';
+import { Controller } from '../../../adapter/input/port/controller';
+import MongoCustomerRepository from '../../../adapter/output/repository/mongodb/create-customer-repository';
+import CreateCustomerOnDb from '../../../core/use-case/create-customer/create-customer-on-db';
+
+export default function makeCreateCostumerController(): Controller {
+    const mongoCustomerRepository = new MongoCustomerRepository();
+    const createCustomerOnDb = new CreateCustomerOnDb(mongoCustomerRepository);
+    return new CreateCustomerController({ createCustomer: createCustomerOnDb });
+}
