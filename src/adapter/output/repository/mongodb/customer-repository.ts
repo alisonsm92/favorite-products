144ef17938cff9c9d3801a3ebd4808f4539f88d7
Alison Machado <alison.sm92@gmail.com>
2021-05-23 18:14:05 -0300

    fix: customer repository

--- src/adapter/output/repository/mongodb/customer-repository.ts
+++ src/adapter/output/repository/mongodb/customer-repository.ts
@@ -1,19 +1,25 @@
+import { Collection, ObjectID } from 'mongodb';
 import CustomerData from '../../../../core/domain/customer-data';
 import CreateCustomerParams from '../../../../core/use-case/create-customer/port/create-customer-params';
-import CustomerRepository from '../../../../core/use-case/port/customer-repository';
+import CreateCustomerRepository from '../../../../core/use-case/create-customer/port/create-customer-reposioty';
 import MongoHelper from './helper/mongodb-helper';
 
-export default class MongoCustomerRepository implements CustomerRepository {
-    async create(data: CreateCustomerParams): Promise<CustomerData['id']> {
-        const customerCollection = await MongoHelper.getCollection('customers');
-        const { insertedId } = await customerCollection.insertOne(data);
+interface CustomerRegister extends Omit<CustomerData, 'id'> {
+    _id: ObjectID
+}
 
-        return insertedId.str;
+export default class MongoCustomerRepository implements CreateCustomerRepository {
+    getCollection(): Collection<CustomerRegister> {
+        return MongoHelper.getCollection('customers');
+    }
+
+    async create(data: CreateCustomerParams): Promise<CustomerData['id']> {
+        const { insertedId } = await this.getCollection().insertOne(data);
+        return insertedId.toString();
     }
 
     async exists(email: CustomerData['email']): Promise<boolean> {
-        const customerCollection = MongoHelper.getCollection('customers');
-        const result = await customerCollection.findOne({ email }, { projection: { _id: 1 } });
+        const result = await this.getCollection().findOne({ email }, { projection: { _id: 1 } });
         return !!result;
     }
 }
