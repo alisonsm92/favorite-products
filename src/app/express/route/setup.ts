413b8ff4b85424ca02b60f31c61fb997b0359db5
Alison Machado <alison.sm92@gmail.com>
2021-05-23 16:38:33 -0300

    ci: config to run integration tests

--- src/app/express/route/setup.ts
+++ src/app/express/route/setup.ts
@@ -5,7 +5,7 @@ export default (app: Express): void => {
     const router = Router();
     app.use('', router);
     readdirSync(`${__dirname}`).map(async (file) => {
-        if (!file.includes('setup.')) {
+        if (!file.includes('setup.') && !file.includes('.test') && !file.includes('.spec')) {
             (await import(`./${file}`)).default(router);
         }
     });
