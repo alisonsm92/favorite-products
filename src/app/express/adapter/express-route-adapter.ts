52b86ad9cbf7d150fb46d5cd23208642ddce2c4c
Alison Machado <alison.sm92@gmail.com>
2021-05-23 21:40:33 -0300

    feat: return req params in express route adapter

--- src/app/express/adapter/express-route-adapter.ts
+++ src/app/express/adapter/express-route-adapter.ts
@@ -3,7 +3,10 @@ import Controller from '../../../adapter/input/port/controller';
 
 export default function adaptRoute(controller: Controller) {
     return async (req: Request, res: Response): Promise<void> => {
-        const request = { body: req.body };
+        const request = {
+            body: req.body,
+            params: req.params,
+        };
         const httpResponse = await controller.handle(request);
         res.status(httpResponse.statusCode).json(httpResponse.body);
     };
