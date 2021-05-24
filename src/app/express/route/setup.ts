55f390a01790f7ce7c362a4f1b15578ecb25f2ed
Alison Machado <alison.sm92@gmail.com>
2021-05-23 23:04:30 -0300

    ci: config vscode debugger

--- src/app/express/route/setup.ts
+++ src/app/express/route/setup.ts
@@ -5,7 +5,7 @@ export default (app: Express): void => {
     const router = Router();
     app.use('', router);
     readdirSync(`${__dirname}`).map(async (file) => {
-        if (!file.includes('setup.') && !file.includes('.test') && !file.includes('.spec')) {
+        if (!new RegExp(['setup', '.map', '.spec', '.test'].join('|')).test(file)) {
             (await import(`./${file}`)).default(router);
         }
     });
