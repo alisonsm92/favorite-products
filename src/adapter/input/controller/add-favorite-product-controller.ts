65acc736b30ddfb5c8bd8fb6f1c50dceef988787
Alison Machado <alison.sm92@gmail.com>
2021-05-29 22:20:12 -0300

    fix: missing controller interface

--- src/adapter/input/controller/add-favorite-product-controller.ts
+++ src/adapter/input/controller/add-favorite-product-controller.ts
@@ -1,11 +1,12 @@
 import AddFavoriteProduct from '../../../core/use-case/add-favorite-product/port/add-favorite-product';
 import AddFavoriteProductParams from '../../../core/use-case/add-favorite-product/port/add-favorite-product-params';
 import { notFound, ok, serverError } from '../helper/http-helper';
+import Controller from '../port/controller';
 import { HttpRequest, HttpResponse } from '../port/http';
 
 type Dependencies = { addFavoriteProduct: AddFavoriteProduct }
 
-export default class AddFavoriteProductController {
+export default class AddFavoriteProductController implements Controller {
     private readonly addFavoriteProduct: AddFavoriteProduct;
 
     constructor({ addFavoriteProduct }: Dependencies) {
